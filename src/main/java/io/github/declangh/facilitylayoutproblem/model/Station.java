fefe047240bc23cf148adc29f544c048785b8dfe
DeclanGH <declanoonunkwo@gmail.com>
2024-09-26 01:41:31 -0400

    create Station constructor that takes Station as param

--- src/main/java/io/github/declangh/facilitylayoutproblem/model/Station.java
+++ src/main/java/io/github/declangh/facilitylayoutproblem/model/Station.java
@@ -16,12 +16,20 @@ public class Station {
 
         switch (stationType) {
             case MANUFACTURING -> this.stationColor = new Color(204, 102, 0);
-            case DISTRIBUTION -> this.stationColor = new Color(204, 204, 0);
+            case DISTRIBUTION -> this.stationColor = new Color(66, 0, 248);
             case SECURITY -> this.stationColor = new Color(9,93,216);
             case HEALTH -> this.stationColor = new Color(57, 255, 20);
             case POWER -> this.stationColor = new Color(128, 128, 128);
+            case HOME -> this.stationColor = new Color(81, 59, 41);
+            case FOOD -> this.stationColor = new Color(247, 255, 0);
             case FIRE -> this.stationColor = new Color(255, 51, 51);
         }
+    }
+
+    public Station(@NotNull Station station) {
+        this.stationId = station.getStationId();
+        this.stationType = station.getStationType();
+        this.stationColor = station.getStationColor();
     }
 
     public StationType getStationType() {
