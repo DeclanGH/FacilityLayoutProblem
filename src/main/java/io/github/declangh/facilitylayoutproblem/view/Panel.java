507a077455641b2bd1d2d1080cabd8a7722fa46e
DeclanGH <declanoonunkwo@gmail.com>
2024-09-26 15:12:42 -0400

    initial working Floor view

--- src/main/java/io/github/declangh/facilitylayoutproblem/view/Panel.java
+++ src/main/java/io/github/declangh/facilitylayoutproblem/view/Panel.java
@@ -1,21 +1,24 @@
 package io.github.declangh.facilitylayoutproblem.view;
 
+import io.github.declangh.facilitylayoutproblem.model.Floor;
+import io.github.declangh.facilitylayoutproblem.model.Hole;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
 
 public class Panel extends JPanel implements ActionListener {
 
     //  500 milliseconds to draw twice a second
     public static final int DRAW_INTERVAL = 500;
 
-    private final AtomicInteger generation;
+    private final Floor floor;
     private final Timer timer;
 
-    public Panel() {
-        this.generation = new AtomicInteger(0);
+    public Panel(Floor floor) {
+        this.floor = floor;
         this.timer = new Timer(DRAW_INTERVAL, this) ;
         timer.start();
     }
@@ -29,9 +32,37 @@ public class Panel extends JPanel implements ActionListener {
     public void paintComponent(final Graphics graphics) {
         super.paintComponent(graphics);
         final Graphics2D graphics2D = (Graphics2D) graphics;
-        graphics2D.setColor(Color.BLACK);
-        graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        graphics2D.drawString("Generation: " + generation.getAndIncrement(), 10, 20);
+
+        graphics2D.setColor(Color.WHITE);
+        graphics2D.fillRect(0, 0, getWidth(), getHeight());
+
+        ArrayList<ArrayList<Hole>> floorGrid = floor.getHoles();
+
+        int numOfRows = floorGrid.size();
+        int numOfColumns = floorGrid.getFirst().size();
+
+        int holeWidth = getWidth() / numOfColumns;
+        int holeHeight = getHeight() / numOfRows;
+
+        //graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        for (int row = 0; row < numOfRows; row++) {
+            for (int col = 0; col < numOfColumns; col++) {
+                Hole hole = floorGrid.get(row).get(col);
+
+                if (hole.isOccupied()) {
+                    graphics2D.setColor(hole.getStation().getColor());
+                } else {
+                    graphics2D.setColor(Color.WHITE);
+                }
+
+                int x = col * holeWidth;
+                int y = row * holeHeight;
+                graphics2D.fillRect(x, y, holeWidth, holeHeight);
+                graphics2D.setColor(Color.WHITE);
+                graphics2D.drawRect(x, y, holeWidth, holeHeight);
+            }
+        }
     }
 
 
